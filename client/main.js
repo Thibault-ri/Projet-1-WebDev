7c4bf1901c522756bed1ea2ac416e8458e999ccb
Thibault <thibault.rieul@gmail.com>
2020-03-18 16:12:25 +0100

    modif sever (local3000)

--- client/main.js
+++ client/main.js
@@ -1,3 +1,26 @@
-import axios from 'axios'
-axios.get('/api/version')
+const axios = require("axios");
 
+axios({
+    "method":"GET",
+    "url":"https://montanaflynn-fifa-world-cup.p.rapidapi.com/teams",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
+    "x-rapidapi-key":"948ca828c0mshe8fd4cb616ef4afp1372fbjsnd5c9fcc4115c",
+    "accepts":"json"
+    }
+    })
+    .then((response)=>{
+      console.log(response)
+      var teams =response.data
+      var html=''
+      for (team of teams){
+          html+=team.id + ' '+team.title+ '<br />'
+      }
+      document.querySelector('#result').innerHTML=html
+    })
+    .catch((error)=>{
+      console.log('Requete retourné avec erreur')
+      console.log(error)
+    })
+    console.log('Apres le code de la requete')
--- server/myapp/index.js
+++ server/myapp/index.js
@@ -2,14 +2,13 @@ const path=require('path')
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use('/', express.static(path.resolve(__dirname,'../../client/dist')))
+
+app.use(function (req, res, next) {
+  res.status(404).sendFile(path.resolve(__dirname,'../../client/404.html'))
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-app.use(function (req, res, next) {
-  res.status(404).sendFile(path.resolve(__dirname,'../../client/404.html'))
-})
