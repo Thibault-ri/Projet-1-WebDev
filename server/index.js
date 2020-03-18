89ed1195f24bbaed022cb821a039a483f3c973e3
Thibault <thibault.rieul@gmail.com>
2020-03-18 13:58:29 +0100

    ajout erreur server 404

--- server/index.js
+++ server/index.js
@@ -8,3 +8,7 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
